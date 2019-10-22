663489d9b5f744a7c6bd5fc17006c88fa9a25758
Ubuntu <ubuntu@ip-172-31-30-176.us-east-2.compute.internal>
2019-10-22 10:47:00 +0000

    MONGO_URL password secure and 'get_recipe' app rendering to recipes.html correctly

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'recipe_collection'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def test():
-    return 'Hello World'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':
