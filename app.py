b64f461c7170d8eef8090f4481db3d18cf192ac5
Ubuntu <ubuntu@ip-172-31-30-176.us-east-2.compute.internal>
2019-10-22 12:29:51 +0000

    card layout for recipe displays in recipes.html

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
-"""from flask_pymongo import PyMongo
-from bson.objectid import ObjectId """
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'recipe_collection'
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
-"""mongo = PyMongo(app)"""
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def test():
-    return 'Hello World'
-
-
-"""@app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
-    return render_template("recipes.html", recipes=mongo.db.recipes.find())"""
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':
