77e35f4db87567a41d779f75a5e271011d2a7276
Ubuntu <ubuntu@ip-172-31-30-176.us-east-2.compute.internal>
2019-10-24 09:49:06 +0000

    images loading from MDB to html - gallery.html - testing page only - ready to integrate to recipes.html

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, send_from_directory
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -15,6 +15,11 @@ mongo = PyMongo(app)
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
     
+
+@app.route('/get_images')
+def get_images():
+    return render_template('gallery.html', images=mongo.db.images.find())
+    
     
 @app.route('/add_recipe')
 def add_recipe():
@@ -27,7 +32,17 @@ def submit_recipe():
     recipes = mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('get_recipes'))
+
     
+"""@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory("images", filename)
+    
+    
+@app.route('/gallery')   
+def get_gallery():
+    image_names = os.listdir('./images')
+    return render_template ("gallery.html", image_names=image_names)"""
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
