703f8bfb75a18ede3639c8a53bec59725b27ce62
Ubuntu <ubuntu@ip-172-31-30-176.us-east-2.compute.internal>
2019-10-25 09:07:22 +0000

    if functionality works well to dynamically populate images to card when recipe created - delete json file, gallery html and Mongo DB images collection

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for, send_from_directory
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'recipe_collection'
@@ -14,13 +14,8 @@ mongo = PyMongo(app)
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
-    
+   
 
-@app.route('/get_images')
-def get_images():
-    return render_template('gallery.html', images=mongo.db.images.find())
-    
-    
 @app.route('/add_recipe')
 def add_recipe():
     return render_template('addrecipe.html',
@@ -33,17 +28,8 @@ def submit_recipe():
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('get_recipes'))
 
-    
-"""@app.route('/upload/<filename>')
-def send_image(filename):
-    return send_from_directory("images", filename)
-    
-    
-@app.route('/gallery')   
-def get_gallery():
-    image_names = os.listdir('./images')
-    return render_template ("gallery.html", image_names=image_names)"""
-    
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
