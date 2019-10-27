5df94aa6e275ac193482732acdf8ec0afe19b47f
Ubuntu <ubuntu@ip-172-31-30-176.us-east-2.compute.internal>
2019-10-27 19:51:44 +0000

    edit recipe synced up correctly - base/recipe html styled with images - images not mobile first tested

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import json
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
@@ -32,6 +33,31 @@ def submit_recipe():
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('get_recipes'))
 
+
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('editrecipe.html', recipe=the_recipe,
+                           categories=all_categories)
+
+
+@app.route('/update_recipe/<recipe_id>', methods=["POST"])
+def update_recipe(recipe_id):
+    recipes = mongo.db.recipes
+    recipes.update( {'_id': ObjectId(recipe_id)},
+    {
+        'category_name':request.form.get('category_name'),
+        'recipe_author':request.form.get('recipe_author'),
+        'recipe_name':request.form.get('recipe_name'),
+        'recipe_date': request.form.get('recipe_date'),
+        'recipe_desc': request.form.get('recipe_desc'),
+        'recipe_ing': request.form.get('recipe_ing'),
+        'recipe_prep': request.form.get('recipe_prep'),
+        'recipe_inst': request.form.get('recipe_inst'),
+        'recipe_notes': request.form.get('recipe_notes')
+    })
+    return redirect(url_for('get_recipes'))
 
 
 if __name__ == '__main__':
