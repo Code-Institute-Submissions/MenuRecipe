a72526ee8aa78dc9a7ad882f265c866efe4cf816
Ubuntu <ubuntu@ip-172-31-30-176.us-east-2.compute.internal>
2019-10-31 10:07:42 +0000

    pip install flask paginate - test pagination for project

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 import json
+import math
 from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
+from flask_paginate import Pagination, get_page_args
+from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
 
 
@@ -17,16 +19,15 @@ mongo = PyMongo(app)
 def get_recipes():
     data = []
     with open("data/dish.json", "r") as json_data:
-        data = json.load(json_data)
+        data = json.load(json_data)    
     return render_template("recipes.html", page_title='Recipes', dish=data, recipes=mongo.db.recipes.find())
-
+                           
 
 @app.route('/add_recipe')
 def add_recipe():
     return render_template('addrecipe.html',
                            categories=mongo.db.categories.find())
                            
-
 
 @app.route('/submit_recipe', methods=['POST'])
 def submit_recipe():
@@ -65,7 +66,7 @@ def update_recipe(recipe_id):
 def delete_recipe(recipe_id):
     mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
     return redirect(url_for('get_recipes'))
-    
+
 
     
 if __name__ == '__main__':
