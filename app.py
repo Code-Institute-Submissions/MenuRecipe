49c91bda153142861ea02a3084eca6a5c265cb52
Ubuntu <ubuntu@ip-172-31-30-176.us-east-2.compute.internal>
2019-11-04 09:38:57 +0000

    updated req.txt with mypongo dependencies

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import json
 import math
 from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
 
 
